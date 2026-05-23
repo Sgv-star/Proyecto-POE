3fcb048e4f17be2fc647d789a664a478afb9f055
Sgv-star <sgonzalezvillada@gmail.com>
2026-05-23 12:48:41 -0500

    Optimización

--- src/Principal.java
+++ src/Principal.java
@@ -7,16 +7,12 @@ import javax.swing.SwingUtilities;
 public class Principal {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // 1. Crear el Modelo
             Campo modeloCampo = new Campo();
 
-            // 2. Crear la Vista
             VistaDuelo vista = new VistaDuelo();
 
-            // 3. Crear el Controlador principal (que orquestará a los demás)
             ControladorDuelo controlador = new ControladorDuelo(modeloCampo, vista);
 
-            // 4. Mostrar la aplicación
             vista.setVisible(true);
         });
     }
